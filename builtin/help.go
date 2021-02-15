0c7b53fb509856782646d39067a1af000b7a89b4
Jacob Simpson <jacob.simpson@gmail.com>
2021-02-15 15:00:33 -0800

    Parameterize the builtin help command.

--- builtin/help.go
+++ builtin/help.go
@@ -7,7 +7,24 @@ import (
 
 type help struct{}
 
-func (*help) Execute([]string) int {
+func (*help) Execute(args []string) int {
+	if len(args) == 0 {
+		return printHelpSummary()
+	}
+	status := 1
+	for _, arg := range args {
+		b := builtins[arg]
+		if b == nil {
+			fmt.Printf("msh: help: no help topics match '%s'.\n", arg)
+			continue
+		}
+		fmt.Printf("%s: %s\n", arg, b.ShortHelp())
+		status = 0
+	}
+	return status
+}
+
+func printHelpSummary() int {
 	names := []string{}
 	for _, b := range builtins {
 		names = append(names, b.Name())
